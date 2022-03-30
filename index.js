b6c666a4b2b42a829127df06877fa8648f14332a
Maseudimo <muhammedalmaseudi99@gmail.com>
2022-03-30 14:07:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express"),
   morgan = require("morgan");
 // integration between your REST API and your database layer using Mongoose
 const mongoose = require("mongoose");
-const Models = require("./modal.js");
+const Models = require("./models.js");
 const Movies = Models.Movie;
 const Users = Models.User;
 // mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true, useUnifiedTopology: true });
